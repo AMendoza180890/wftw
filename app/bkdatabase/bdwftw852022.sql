b30012e634bfc803a5e044ff1716fe2b10f2bc37
Allan Mendoza <allanmaleman@gmail.com>
2022-05-08 16:13:25 -0600

    Se agrega el campo de insitucion o de donde proviene el beneficiario si es de Tesoros o de las asambleas de Dios

--- app/bkdatabase/bdwftw852022.sql
+++ app/bkdatabase/bdwftw852022.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 27-04-2022 a las 03:45:36
+-- Tiempo de generación: 09-05-2022 a las 00:11:45
 -- Versión del servidor: 10.4.21-MariaDB
 -- Versión de PHP: 8.0.11
 
